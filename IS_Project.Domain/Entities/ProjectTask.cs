9a489fcbb50a5d6dde5222ae3484b81bb4da0c63
Veeksy <63778404+Veeksy@users.noreply.github.com>
2024-12-18 15:03:51 +0400

    1

--- IS_Project.Domain/Entities/ProjectTask.cs
+++ IS_Project.Domain/Entities/ProjectTask.cs
@@ -21,19 +21,19 @@ public class ProjectTask
     /// <summary>
     /// Время на работу
     /// </summary>
-    public decimal? EstablishedTime { get; set; }
+    public decimal EstablishedTime { get; set; } = decimal.Zero;
     /// <summary>
     /// Затраченное время
     /// </summary>
-    public decimal? SpentTime { get; set; }
+    public decimal SpentTime { get; set; } = decimal.Zero;
     /// <summary>
     /// айди исполнителя
     /// </summary>
-    public Guid? PerformerId { get; set; }
+    public Guid PerformerId { get; set; } = default!;
     /// <summary>
     /// айди проекта
     /// </summary>
-    public Guid? ProjectId { get; set; }
+    public required Guid ProjectId { get; set; }
     /// <summary>
     /// Статус задачи
     /// </summary>
---
+++ IS_Project.Infrastructure/Data/Configuration/ProjectTaskConfiguration.cs
@@ -0,0 +1,23 @@
+﻿using IS_Project.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace IS_Project.Infrastructure.Data.Configuration;
+
+public class ProjectTaskConfiguration : IEntityTypeConfiguration<ProjectTask>
+{
+    public void Configure(EntityTypeBuilder<ProjectTask> builder)
+    {
+        builder.HasKey(x=> x.Id);
+
+        builder.Property(x=>x.Name)
+            .HasMaxLength(150)
+            .IsRequired();
+
+        builder.Property(x => x.ProjectId)
+            .IsRequired();
+
+        builder.Property(x => x.TaskStatus)
+            .HasDefaultValue(TaskStatus.Created);
+    }
+}
